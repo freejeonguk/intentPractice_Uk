abd361fbe146e398ae06757d95a6b9feb3f07694
freejeonguk <117013897+freejeonguk@users.noreply.github.com>
2022-11-11 19:38:34 +0900

    닉네임 입력용 별개 화면 추가

---
+++ app/src/main/java/com/thirdparty/intentpractice_uk/EditNicknameActivity.kt
@@ -0,0 +1,11 @@
+package com.thirdparty.intentpractice_uk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+    }
+}
